088b0376c13d3d749edcd11059ccdd86d749dd04
liu <2294397905@qq.com>
2022-09-28 22:43:59 +0800

    定义模型

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class BookInfo(models.Model):
+    # 创建字段，字段类型...
+    name = models.CharField(max_length=20, verbose_name='名称')
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=20, verbose_name='名称')
+    gender = models.SmallIntegerField()
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE, verbose_name='图书')  # 外键
